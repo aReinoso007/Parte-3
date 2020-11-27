3a6271ac65d13d778ecc36d1bc8606eebc1e3c6c
Alex Reinoso <ralexjessiel@gmail.com>
2020-11-26 22:07:15 -0500

    Capturar video desde camara

--- Principal.cpp
+++ Principal.cpp
@@ -1,9 +1,28 @@
 #include "Conversor.hpp"
 
 int main(int argc, char *argv[]){
-
+    
+    VideoCapture video(0);
     
 
+    namedWindow("video", WINDOW_AUTOSIZE);
 
+    if(video.isOpened()){
+        Mat frame;
+        namedWindow("Video",WINDOW_AUTOSIZE);
+        while(3==3){
+            video >> frame;
+            
+            if(frame.rows<=0 || frame.cols<=0)
+                break;
+
+            imshow("Video", frame);
+
+            
+            if(waitKey(23)==27) //Pedimos que espere 23 milisegundos a que el usario presione la tecla escape
+                break;
+        }
+        destroyAllWindows();
+    }
     return 0;
 }
