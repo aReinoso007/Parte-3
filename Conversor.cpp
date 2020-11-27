dd99af32717f7b7c6633965f7df1d8dff96317b5
Alex Reinoso <ralexjessiel@gmail.com>
2020-11-26 22:19:51 -0500

    Metodos de conversion de espacios de color

--- Conversor.cpp
+++ Conversor.cpp
@@ -7,3 +7,15 @@ Conversor::Conversor(){
 void Conversor::hsv(cv::Mat src, cv::Mat dest){
 
 }
+
+void Conversor::toGray(Mat src, Mat dest){
+
+}
+
+void Conversor::toLAb(){
+
+}
+
+void Conversor::toYcrCb(){
+    
+}
